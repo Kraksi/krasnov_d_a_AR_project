7f94b00d024b668226ab3a62dd72d46bdf877816
Krasti <krasnov.d.a.20@mail.ru>
2026-03-11 22:51:19 +0300

    Добавлены противники

---
+++ Assets/Scripts/Homework_7/EnemyAI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform Player;
+    public float UpdateRate = 0.2f;
+
+    private NavMeshAgent _agent;
+    private float _timer;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= UpdateRate)
+        {
+            _timer = 0f;
+
+            if (Player != null)
+                _agent.SetDestination(Player.position);
+        }
+    }
+}
